854fa70fbda2fe1104b2030008037e6df53b762d
someone_ <skelfnej8592@gmail.com>
2024-08-17 19:47:48 +0900

    add Cell.index

---
+++ Assets/Scripts/Effect.cs
@@ -0,0 +1,16 @@
+using System;
+
+public abstract class Effect {
+    public abstract void effect()
+}
+
+partial class 
+
+public class BackEffect: Effect {
+    int back;
+
+    public override void effect(Cell cell)
+    {
+        return cell.id
+    }
+}

368a1f2724caf35c38733957d02bbe121fb7b570
kuma807 <kumakurahideto@kumakuranoMacBook-Pro.local>
2024-08-17 18:07:21 +0900

    add DrawEnemy

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -6,6 +6,7 @@ public class GameController : MonoBehaviour
 {
     private Cell[] board;
     public GameObject enemy;
+    private List<GameObject> instantiatedEnemies = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +19,7 @@ public class GameController : MonoBehaviour
             new Cell(6, 0, 0),
             new Cell(8, 0, 0)
         };
+        DrawEnemy();
     }
 
     // Update is called once per frame
@@ -43,13 +45,26 @@ public class GameController : MonoBehaviour
             outputString += " " + board[i].enemyNumber + ",";
         }
         Debug.Log(outputString);
+        DrawEnemy();
     }
 
     void DrawEnemy()
     {
+        foreach (GameObject enemy in instantiatedEnemies)
+        {
+            if (enemy != null)
+            {
+                Destroy(enemy);
+            }
+        }
+        instantiatedEnemies.Clear();
         for (int i = 0; i < board.Length; i++)
         {
-            
+            if (board[i].enemyNumber != 0)
+            {
+                GameObject enemyInstance = Instantiate(enemy, new Vector3(board[i].x, board[i].y, 0), Quaternion.identity);
+                instantiatedEnemies.Add(enemyInstance);
+            }
         }
     }
 }

763f6b096526af7b91bdcc73da02bd72c460f84e
m1zz1y <ruaces@gmail.com>
2024-08-17 20:29:28 +0900

    add Stage, Board class.

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+namespace GMTKGameJam.Scripts;
+
+[System.Diagnostics.DebuggerDisplay("cells: {cells}")]
+public class Board
+{
+    public []Cell cells;
+
+    public Board([] Cell _cells)
+    {
+        cells = _cells;
+    }
+
+    public void DisplayInfo()
+    {
+        Debug.Log(this);
+    }
+}

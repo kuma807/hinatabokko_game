5a7f6a75a19e0d08b06bf39c5bd431a7829c3595
someone_ <skelfnej8592@gmail.com>
2024-08-18 20:05:58 +0900

    add iroiro

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 [System.Diagnostics.DebuggerDisplay("cells: {cells}")]
 public class Board: List<Cell>
 {
+    public List<int> start, goal;
+    public Board() {}
     public void DisplayInfo()
     {
         UnityEngine.Debug.Log(this);

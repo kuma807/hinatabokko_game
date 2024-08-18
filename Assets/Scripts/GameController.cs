b21fc22c98bb2767b65bff5ce4ad62e9e3121cf0
kuma807 <kumakurahideto@kumakuranoMacBook-Pro.local>
2024-08-18 19:10:21 +0900

    delete enemy number, change enemy size

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using Unity.VisualScripting;
+using System;
 
 public class GameController : MonoBehaviour
 {
@@ -22,10 +23,10 @@ public class GameController : MonoBehaviour
         Board board = new Board()
         {
             new Cell(0, 0, new Enemy(1, new List<int>{1, 2, 3, 4, 5, 6}, 0), 0, new NullStepOnEffect(), new NullRollDiceEffect()),
-            new Cell(2, 0, new Enemy(0, new List<int>{1, 2, 3, 4, 5, 6}, 0), 1, new NullStepOnEffect(), new NullRollDiceEffect()),
-            new Cell(4, 0, new Enemy(0, new List<int>{1, 2, 3, 4, 5, 6}, 0), 2, new NullStepOnEffect(), new NullRollDiceEffect()),
-            new Cell(6, 0, new Enemy(0, new List<int>{1, 2, 3, 4, 5, 6}, 0), 3, new NullStepOnEffect(), new NullRollDiceEffect()),
-            new Cell(8, 0, new Enemy(0, new List<int>{1, 2, 3, 4, 5, 6}, 0), 4, new NullStepOnEffect(), new NullRollDiceEffect()),
+            new Cell(2, 0, new Enemy(100, new List<int>{1, 2, 3, 4, 5, 6}, 1), 1, new NullStepOnEffect(), new NullRollDiceEffect()),
+            new Cell(4, 0, new Enemy(1000, new List<int>{1, 2, 3, 4, 5, 6}, 0), 2, new NullStepOnEffect(), new NullRollDiceEffect()),
+            new Cell(6, 0, new Enemy(10000, new List<int>{1, 2, 3, 4, 5, 6}, 0), 3, new NullStepOnEffect(), new NullRollDiceEffect()),
+            new Cell(8, 0, new Enemy(1000000000, new List<int>{1, 2, 3, 4, 5, 6}, 0), 4, new NullStepOnEffect(), new NullRollDiceEffect()),
         };
         stage = new Stage(enemies, board);
         DrawEnemy(ref stage.board);
@@ -77,16 +78,10 @@ public class GameController : MonoBehaviour
         {
             if (board[i].enemy.count != 0)
             {
-                GameObject enemyInstance = Instantiate(enemyObjects[board[i].enemy.id], new Vector3(board[i].x, board[i].y + (float)(0.3 - 0.3 * board[i].enemy.id), 0), Quaternion.identity);
-                TextMeshProUGUI textMeshPro = enemyInstance.GetComponentInChildren<TextMeshProUGUI>();
-                if (textMeshPro != null)
-                {
-                    textMeshPro.text = board[i].enemy.count.ToString();  // テキストを更新
-                }
-                else
-                {
-                    Debug.LogWarning("TextMeshProUGUI component not found in the prefab's children.");
-                }
+                float scale = (float)2.0 + (float)Math.Log((double)board[i].enemy.count, 10.0) / (float)2.0;
+                float enemyHight = enemyObjects[board[i].enemy.id].GetComponent<SpriteRenderer>().bounds.size.y;
+                GameObject enemyInstance = Instantiate(enemyObjects[board[i].enemy.id], new Vector3(board[i].x, board[i].y + (float)(enemyHight * scale / 2.0) - (float)0.4, 0), Quaternion.identity);
+                enemyInstance.transform.localScale = new Vector3(scale, scale, scale);
                 instantiatedEnemies.Add(enemyInstance);
             }
         }
